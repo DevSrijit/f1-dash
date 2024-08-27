53531d2e286d75b0ad40615ec2db51c7e30c91b1
devsrijit <srijitghosh7331@gmail.com>
2024-08-28 02:33:45 +0530

    chore: Remove commented out code and unused footer links

--- dash/src/app/(nav)/layout.tsx
+++ dash/src/app/(nav)/layout.tsx
@@ -13,15 +13,13 @@ export default function Layout({ children }: Props) {
 			<div className="sticky left-0 top-0 z-10 flex h-12 w-full items-center justify-between gap-4 border-b border-zinc-800 bg-black p-2">
 				<Menubar />
 
-				<div className="hidden items-center gap-4 pr-2 sm:flex">
-					<IconLabelButton icon="bmc" href="https://www.buymeacoffee.com/slowlydev">
-						Coffee
-					</IconLabelButton>
-
-					<IconLabelButton icon="github" href="https://github.com/slowlydev/f1-dash">
+				{
+				/*<div className="hidden items-center gap-4 pr-2 sm:flex">
+					<IconLabelButton icon="github" href="https://github.com/devsrijit/f1-dash">
 						GitHub
 					</IconLabelButton>
-				</div>
+				</div>*/
+				}
 			</div>
 
 			{children}
