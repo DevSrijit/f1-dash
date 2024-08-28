7a3fc16299b3d95b8dfb2b6ce13061fff2d401fa
devsrijit <srijitghosh7331@gmail.com>
2024-08-28 22:09:38 +0530

    Revert "chore: Update ORIGIN environment variable to allow multiple origins"
    This reverts commit cffb088d2a68afec4f39415689432fed2aee01b0.

--- crates/live/src/server/cors.rs
+++ crates/live/src/server/cors.rs
@@ -2,16 +2,9 @@ use axum::http::{HeaderValue, Method};
 use tower_http::cors::CorsLayer;
 
 pub fn init() -> CorsLayer {
-    let origins = std::env::var("ORIGIN").expect("no origin env found");
-    let origins: Vec<HeaderValue> = origins
-        .split(',')
-        .map(|origin| origin.trim().parse::<HeaderValue>().unwrap())
-        .collect();
+    let origin = std::env::var("ORIGIN").expect("no origin env found");
 
-    let mut cors = CorsLayer::new();
-    for origin in origins {
-        cors = cors.allow_origin(origin);
-    }
-
-    cors.allow_methods([Method::GET, Method::CONNECT])
+    CorsLayer::new()
+        .allow_origin(origin.parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::CONNECT])
 }
